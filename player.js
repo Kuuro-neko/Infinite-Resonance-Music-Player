ba30e90121d4e9283949f9677724d8c92567970d
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-01-25 22:17:39 +0100

    Style picker and some css

--- player.js
+++ player.js
@@ -6,6 +6,17 @@ let looped = false;
 let shuffling = false;
 let stylesToPlay;
 
+let duration = 64000;
+let playing = false;
+let progress = 0;
+let startTime;
+
+let styleColors = [
+    '#1fb52b', // MED
+    '#e823db', // MO
+    '#e68b15' // SF
+];
+
 function randomsizeStyles() {
     stylesToPlay = [];
     for (let i = 0; i < 4; i++) {
@@ -13,6 +24,43 @@ function randomsizeStyles() {
     }
 }
 randomsizeStyles();
+
+function changeLayerStyle(layer, style) {
+    stylesToPlay[layer] = style;
+}
+
+function changeStylesFromUserInput() {
+    let melo1Select = document.getElementById('melo1').getElementsByTagName('select')[0];
+    let melo2Select = document.getElementById('melo2').getElementsByTagName('select')[0];
+    let percSelect = document.getElementById('perc').getElementsByTagName('select')[0];
+    let accSelect = document.getElementById('acc').getElementsByTagName('select')[0];
+    changeLayerStyle(Layers.MEL_1, melo1Select.selectedIndex);
+    changeLayerStyle(Layers.MEL_2, melo2Select.selectedIndex);
+    changeLayerStyle(Layers.PERC, percSelect.selectedIndex);
+    changeLayerStyle(Layers.ACC, accSelect.selectedIndex);
+    enableStyles(stylesToPlay);
+}
+
+function changeUserInputFromShuffle() {
+    let melo1Select = document.getElementById('melo1').getElementsByTagName('select')[0];
+    let melo2Select = document.getElementById('melo2').getElementsByTagName('select')[0];
+    let percSelect = document.getElementById('perc').getElementsByTagName('select')[0];
+    let accSelect = document.getElementById('acc').getElementsByTagName('select')[0];
+    melo1Select.selectedIndex = stylesToPlay[Layers.MEL_1];
+    melo2Select.selectedIndex = stylesToPlay[Layers.MEL_2];
+    percSelect.selectedIndex = stylesToPlay[Layers.PERC];
+    accSelect.selectedIndex = stylesToPlay[Layers.ACC];
+
+    let melo1Color = styleColors[stylesToPlay[Layers.MEL_1]];
+    let melo2Color = styleColors[stylesToPlay[Layers.MEL_2]];
+    let percColor = styleColors[stylesToPlay[Layers.PERC]];
+    let accColor = styleColors[stylesToPlay[Layers.ACC]];
+
+    document.getElementById('melo1').style.backgroundColor = melo1Color;
+    document.getElementById('melo2').style.backgroundColor = melo2Color;
+    document.getElementById('perc').style.backgroundColor = percColor;
+    document.getElementById('acc').style.backgroundColor = accColor;
+}
 
 const Styles = {
     MED: 0,
@@ -28,18 +76,26 @@ const Layers = {
 };
 
 function pauseSounds() {
+    playing = false;
     for (let i = 0; i < sounds.length; i++) {
         sounds[i].pause();
     }
 }
 
 function playSounds() {
+    if (playing) {
+        stopSounds();
+    }
+    playing = true;
+    progress = 0;
+    startTime = new Date().getTime();
     for (let i = 0; i < sounds.length; i++) {
         sounds[i].play();
     }
 }
 
 function stopSounds() {
+    playing = false;
     console.log("Stopping sounds");
     for (let i = 0; i < sounds.length; i++) {
         sounds[i].stop();
@@ -84,6 +140,7 @@ function enableStyles(stylesToPlay) {
         sounds[getSoundIndex(stylesToPlay[i], i)].volume(volume);
     }
     console.log("Styles enabled: " + stylesToPlay);
+    changeUserInputFromShuffle();
 }
 
 function updateLoader() {
@@ -179,3 +236,13 @@ sounds[0].on('end', function () {
         playSounds();
     }
 });
+
+// run a function every 100ms to update the progress bar
+setInterval(function () {
+    if (playing) {
+        if (progress <= 100) {
+            progress = (new Date().getTime() - startTime) / duration * 100;
+            document.getElementById('progress').style.width = `${progress}%`;
+        }
+    }
+}, 100);
